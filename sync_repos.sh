cba373dcc2746cca89b4e11909b683abeed0ef80
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2014-03-01 15:28:12 -0500

    Removing debuginfo as well

--- sync_repos.sh
+++ sync_repos.sh
@@ -3,24 +3,25 @@
 mkdir -p ./repo/centos/6.5/
 rsync --progress -h -av --delete --delete-excluded --exclude "SRPMS" \
   --exclude="ppc64" --exclude "local*" --exclude "isos" --exclude "i386" \
-  --exclude="SCL" --exclude="xen4" rsync://mirrors.kernel.org/centos/6.5/ \
-  ./repo/centos/6.5/
+  --exclude="SCL" --exclude="xen4" --exclude="*debuginfo*.rpm" \
+  rsync://mirrors.kernel.org/centos/6.5/ ./repo/centos/6.5/
 
 mkdir -p ./repo/epel/6/
 rsync --progress -h -av --delete --delete-excluded --exclude "SRPMS" \
   --exclude="ppc64" --exclude "local*" --exclude "isos" --exclude "i386" \
+  --exclude="*debuginfo*.rpm" \
   rsync://mirror.pnl.gov/epel/6/ ./repo/epel/6/
 
 mkdir -p ./repo/puppet/
 rsync --progress -h -avxH --exclude "apt*" --exclude "i386" --exclude "SRPMS" \
   --exclude "fedora" --exclude "5" --exclude "devel" --exclude 'yumclear' \
-  --prune-empty-dirs --delete --delete-excluded \
+  --prune-empty-dirs --delete --delete-excluded --exclude="*debuginfo*.rpm" \
   rsync://yum.puppetlabs.com/packages/ ./repo/puppet/
 
 mkdir -p ./repo/postgresql/9.3/
 rsync --progress -h -av --delete --delete-excluded --exclude "SRPMS" \
   --exclude="ppc64" --exclude "local*" --exclude "isos" --exclude "*i386" \
-  --exclude="fedora*" --exclude="rhel-5*" \
+  --exclude="fedora*" --exclude="rhel-5*" --exclude="*debuginfo*.rpm" \
   rsync://yum.postgresql.org/pgrpm-93/ ./repo/postgresql/9.3/
 
 if [ ! -f RPM-GPG-KEY-PGDG-93 ]; then
